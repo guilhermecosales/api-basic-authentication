85f8e16c24eb83ccb44cc66aa2f2058b2dc99711
Guilherme Costa Sales <guilhermecosales@gmail.com>
2022-12-30 13:19:09 -0300

    feat: add not found exception

---
+++ src/main/java/br/com/github/apibasicauthentication/service/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.github.apibasicauthentication.service.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
