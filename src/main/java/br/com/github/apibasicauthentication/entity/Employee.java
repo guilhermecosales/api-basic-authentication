9724a84405b959a6998d4600572148e5eab479bd
Guilherme Costa Sales <guilhermecosales@gmail.com>
2022-12-30 12:00:10 -0300

    feat: entity and DTO definition

---
+++ src/main/java/br/com/github/apibasicauthentication/entity/Employee.java
@@ -0,0 +1,41 @@
+package br.com.github.apibasicauthentication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+}
---
+++ src/main/java/br/com/github/apibasicauthentication/entity/EmployeeDto.java
@@ -0,0 +1,23 @@
+package br.com.github.apibasicauthentication.entity;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * A DTO for the {@link Employee} entity
+ */
+
+@Data
+public class EmployeeDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -3760445487636086034L;
+
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final String userName;
+
+}
