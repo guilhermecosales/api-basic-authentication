c00c33a3f08f2a9b8443777dfb4efa54e9133fad
Guilherme Costa Sales <guilhermecosales@gmail.com>
2022-12-30 11:30:51 -0300

    feat: project creation

---
+++ src/main/java/br/com/github/apibasicauthentication/ApiBasicAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.apibasicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBasicAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/github/apibasicauthentication/ApiBasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.apibasicauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBasicAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
