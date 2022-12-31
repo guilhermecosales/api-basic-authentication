6a7b24faf6d58cdd20ad58c402850b39d6a3cb07
Guilherme Costa Sales <guilhermecosales@gmail.com>
2022-12-31 08:11:11 -0300

    feat: adds security configuration

---
+++ src/main/java/br/com/github/apibasicauthentication/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package br.com.github.apibasicauthentication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "api/account/**")
+                .permitAll()
+                .anyRequest()
+                .hasAnyRole("USER", "ADMIN")
+                .and()
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+}
