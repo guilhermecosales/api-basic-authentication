2727f1a064f7b4f33b3ddf37681947c3535d6615
Guilherme Costa Sales <guilhermecosales@gmail.com>
2022-12-31 08:11:48 -0300

    feat: adds account and role domains

---
+++ src/main/java/br/com/github/apibasicauthentication/entity/Account.java
@@ -0,0 +1,47 @@
+package br.com.github.apibasicauthentication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean enabled = true;
+    private boolean credentialsExpired = false;
+    private boolean expired = false;
+    private boolean locked = false;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "AccountRole",
+            joinColumns = @JoinColumn(name = "accountId", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roleId", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(id, account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
---
+++ src/main/java/br/com/github/apibasicauthentication/entity/Role.java
@@ -0,0 +1,23 @@
+package br.com.github.apibasicauthentication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+
+}
