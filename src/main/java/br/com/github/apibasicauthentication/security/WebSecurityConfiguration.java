0025cac2689104e85e7140d527e3d41b5f200a28
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-01-01 02:43:41 -0300

    feat: adds authentication provider

--- src/main/java/br/com/github/apibasicauthentication/security/WebSecurityConfiguration.java
+++ src/main/java/br/com/github/apibasicauthentication/security/WebSecurityConfiguration.java
@@ -1,9 +1,11 @@
 package br.com.github.apibasicauthentication.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,10 +13,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfiguration {
+
+    private final AccountAuthenticationProvider accountAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(accountAuthenticationProvider);
+
         http
                 .csrf()
                 .disable()
