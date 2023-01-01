7f4f79bbb9726c20b4d51e9aeed07fcbe1cabdc2
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-01-01 01:47:03 -0300

    refactor: renames to WebSecurityConfiguration

--- src/main/java/br/com/github/apibasicauthentication/security/WebSecurityConfiguration.java
+++ src/main/java/br/com/github/apibasicauthentication/security/WebSecurityConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
+public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
