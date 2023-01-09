c4d6c82a723daeba5a9f07501c42eb1b6f9fa061
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-01-09 08:22:15 -0300

    feat: update authentication filter

--- src/main/java/br/com/github/apibasicauthentication/security/WebSecurityConfiguration.java
+++ src/main/java/br/com/github/apibasicauthentication/security/WebSecurityConfiguration.java
@@ -3,13 +3,14 @@ package br.com.github.apibasicauthentication.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +23,12 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
+                .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "api/account/**")
+                .requestMatchers(POST, "/api/account/**")
                 .permitAll()
+                .requestMatchers("/**")
+                .authenticated()
                 .anyRequest()
                 .hasAnyRole("USER", "ADMIN")
                 .and()
